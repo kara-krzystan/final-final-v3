e35b48ebe61795364236a165e5644dba210898a1
Kara Krzystan <kara.krzystan@gmail.com>
2021-04-14 04:50:01 -0700

    Finish schemas and test

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,77 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Recipe } = require('../models');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello world!';
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('thoughts');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('thoughts');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('thoughts');
+    },
+    recipes: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Recipe.find(params).sort({ createdAt: -1 });
+    },
+    recipe: async (parent, { _id }) => {
+      return Recipe.findOne({ _id });
     }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addRecipe: async (parent, args, context) => {
+      if (context.user) {
+        const recipe = await Recipe.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { recipe: recipe._id } },
+          { new: true }
+        );
+
+        return recipe;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
   }
 };
 
--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,9 +1,37 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
-  }
+type User {
+  _id: ID
+  username: String
+  email: String
+  recipes: [Recipe]
+}
+
+type Recipe {
+  _id: ID
+  recipeText: String
+  username: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  me: User
+  users: [User]
+  user(username: String!): User
+  recipes(username: String): [Recipe]
+  recipe(_id: ID!): Recipe
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  addRecipe(recipeText: String!): Recipe
+}
 `;
 
 module.exports = typeDefs;
